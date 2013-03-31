b5e4bee88ef76a616a9c985a95c2cacfa5c925d2
Luis Antonio Perez <luisperez@college.harvard.edu>
2013-03-31 02:21:51 -0400

    Testing branch. Added file with names

---
+++ matrix.ml
@@ -0,0 +1,16 @@
+(* PROJECT: OCaml Matrix/Min_Mathematica 
+ *
+ * NAMES:
+ *
+ * Partner 1's name: Andy Shi
+ * Partner 1's github username: shiandy
+ *
+ * Partner 2's name: Ding Zhou
+ * Partner 2's code.seas account: dzhou94
+  *
+ * Partner 3's name: Jason Wang
+ * Partner 3's code.seas account: jasonwang99
+  *
+ * Partner 4's name: Luis Perez
+ * Partner 4's code.seas account: kandluis
+ *)
